0962416b576eba324dfb06bfe58ed7975e88f10f
Usama Subhani <reachusama@protonmail.com>
2020-06-12 20:14:10 +0500

    Fetch global data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Chart from './components/Chart'
 import Table from './components/Table'
 
 function App() {
+  getGlobalFigures()
+  getGlobalHistory()
   return (
     <div className="App">
       <Numbers />
@@ -13,6 +15,24 @@ function App() {
       <Table />
     </div>
   );
+}
+
+const numbersUrl = "https://corona.lmao.ninja/v2/all";
+const globalDataUrl = 'https://covidapi.info/api/v1/global/count'
+
+async function getGlobalFigures() {
+  await fetch(numbersUrl)
+  .then(response => response.json())
+  .then(data => {
+      console.log(data);
+  })
+}
+async function getGlobalHistory() {
+  fetch(globalDataUrl)
+  .then(response => response.json())
+  .then(data => {
+      console.log(data)
+  })
 }
 
 export default App;
